bdd09bed758303478fadca766f7dead2336535de
Daniil <Comm2k3@hotmail.com>
2022-12-25 03:34:23 +0200

    hw done

--- Program.cs
+++ Program.cs
@@ -56,3 +56,49 @@
 //     resultCol = sumCol / rows;
 //     Console.WriteLine("Sum of " + (i + 1) + " column: " + sumCol + " " + Math.Round(resultCol,1));
 // }
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+// Console.WriteLine("Введите номер элемента строки");
+// int rowPos = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите номер элемента столбца");
+// int colPos = Convert.ToInt32(Console.ReadLine());
+
+// int rows, cols;
+// bool result1 = false;
+// bool result2 = false;
+
+// //Initialize matrix a  
+// int[,] a = {
+//     {1, 4, 7, 2},
+//     {5, 9, 2, 3},
+//     {8, 4, 2, 4}
+// };
+
+// rows = a.GetLength(0);
+// cols = a.GetLength(1);
+
+// for (int i = 0; i < rows; i++)
+// {
+//     for (int j = 0; j < cols; j++)
+//     {
+//         if (j == colPos)
+//         {
+//             result1 = true;
+//         } 
+//     } 
+//     if (i == rowPos)
+//     {
+//         result2 = true;
+//     } 
+// }
+// if (result1 && result2 == true)
+// {
+//     Console.WriteLine($"{rowPos}{colPos} элемент с такой позицией в массиве есть, это число {a[rowPos,colPos]}");
+// } else Console.WriteLine($"{rowPos}{colPos} элемента с такой позицией в массиве нет");
